c30914d4b6558abfc8baf050f51abc974724f882
casc1244 <1356602646@qq.com>
2020-04-10 15:27:24 +0800

    4102.cpp

--- 4102/4102.cpp
+++ 4102/4102.cpp
@@ -1,20 +1,50 @@
-﻿// 4102.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿#include <opencv2/opencv.hpp>
+#include <vector>
 #include <iostream>
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat Matgray;
+	cv::Mat Mat_binary;
+	cv::Mat Mat_canny;
+	cv::Mat srcImage = imread("D:\\360downloads\\9.png");
+
+	cvtColor(srcImage, Matgray, COLOR_BGR2GRAY);//转化边缘检测后的图为灰度图
+	cv::Mat elementRect;
+	elementRect = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3), cv::Point(-1, -1));
+	cv::morphologyEx(Matgray, Matgray, cv::MORPH_CLOSE, elementRect);
+    // binary
+	cv::threshold(Matgray, Mat_binary, 125, 255.0, cv::THRESH_BINARY);
+	// detect edge
+	cv::Canny(Mat_binary, Mat_canny, 50, 125, 3);
+	std::vector<cv::Vec2f> lines;
+	cv::HoughLines(Mat_canny, lines, 1, CV_PI / 180, 100);
+
+	cv::Mat lineMat;
+	cv::HoughLines(Mat_canny, lineMat, 1, CV_PI / 180, 100);
+	//float r = lines[0][0];
+	//float t = lines[0][1];
+
+	// draw line
+	std::vector<cv::Vec2f>::iterator it = lines.begin();
+	for (; it != lines.end(); ++it)
+	{
+		float rho = (*it)[0], theta = (*it)[1];
+		cv::Point pt1, pt2;
+		double a = cos(theta), b = sin(theta);
+		double x0 = a * rho, y0 = b * rho;
+		pt1.x = cv::saturate_cast<int>(x0 + 1000 * (-b));
+		pt1.y = cv::saturate_cast<int>(y0 + 1000 * (a));
+		pt2.x = cv::saturate_cast<int>(x0 - 1000 * (-b));
+		pt2.y = cv::saturate_cast<int>(y0 - 1000 * (a));
+		cv::line(srcImage, pt1, pt2, cv::Scalar(0, 0, 255), 1, CV_AA);
+	}
+
+	cv::imshow("canny", Mat_canny);
+	cv::imshow("mat", srcImage);
+	cv::waitKey(0);
+
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
